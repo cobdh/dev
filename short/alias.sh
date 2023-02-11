651255f2367404b1fa4e8996f2a79ecf879693e3
Helmut Konrad Schewe <helmutus@outlook.com>
2023-02-11 15:44:10 +0000

    chore(alias): add some shortcuts
    See: cobdh/dev#42

--- short/alias.sh
+++ short/alias.sh
@@ -1,5 +1,8 @@
 cd $COBDH
 
-alias fdev="cd ${COBDH}/dev"
-alias fpla="cd ${COBDH}/plan"
 alias fapp="cd ${COBDH}/app"
+alias fdat="cd ${COBDH}/data"
+alias fdev="cd ${COBDH}/dev"
+alias fndb="cd ${COBDH}/nano_db"
+alias fnpu="cd ${COBDH}/nano_pub"
+alias fpla="cd ${COBDH}/plan"

d0775290d683159588363a7a5e3207f1f6a55a7b
Helmut Konrad Schewe <helmutus@outlook.com>
2023-03-06 18:12:16 +0000

    feat(xml): add option to format different files

--- cobdh/xml/cli.py
+++ cobdh/xml/cli.py
@@ -1,3 +1,4 @@
+import argparse
 import pathlib
 import sys
 
@@ -6,7 +7,8 @@ import cobdh.xml.inter
 
 
 def main() -> int:
-    items = files()
+    path, exts = evalcli()
+    items = files(path, exts=exts)
     print('detected:')
     for path in items:
         print(path)
@@ -27,11 +29,35 @@ def main() -> int:
     return cobdh.utils.SUCCESS
 
 
-def files():
-    result = []
-    path = sys.argv[-1] if len(sys.argv) > 1 else '.'
+def files(path, exts: str = 'xml'):
     path = pathlib.Path(path)
     result = [path]
     if not path.is_file():
-        result = list(path.glob('**/*.xml'))
+        result = []
+        for ext in exts.split():
+            result += list(path.glob(f'**/*.{ext}'))
     return result
+
+
+def evalcli():
+    parser = argparse.ArgumentParser(
+        prog='cob_xml',
+        description='Format xml files.',
+    )
+    parser.add_argument(
+        'src',
+        help='directory or a single file',
+        default='.',
+        nargs='?',
+    )
+    parser.add_argument(
+        '--ext',
+        help='select file ext',
+        nargs='?',
+        default='xml'.split(),
+        action='append',
+    )
+    args = parser.parse_args()
+    path = args.src
+    exts = ' '.join(list(set(args.ext)))
+    return path, exts

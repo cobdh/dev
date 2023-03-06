938840e1fa9013a89454cc6862dc251a02f1f810
Helmut Konrad Schewe <helmutus@outlook.com>
2023-03-06 12:50:31 +0000

    feat(utils): add newline constant

--- cobdh/utils.py
+++ cobdh/utils.py
@@ -4,6 +4,7 @@ import subprocess
 
 SUCCESS = 0
 FAILURE = 1
+NEWLINE = '\n'
 
 
 def file_read(path: str) -> str:
@@ -11,20 +12,20 @@ def file_read(path: str) -> str:
     >>> file_read(__file__)
     '...'
     """
-    with open(path, mode='r', encoding='utf8', newline='\n') as fp:
+    with open(path, mode='r', encoding='utf8', newline=NEWLINE) as fp:
         return fp.read()
 
 
 def file_replace(path: str, content='') -> str:
-    content = content.rstrip() + '\n'
-    with open(path, mode='w', encoding='utf8', newline='\n') as fp:
+    content = content.rstrip() + NEWLINE
+    with open(path, mode='w', encoding='utf8', newline=NEWLINE) as fp:
         fp.write(content)
 
 
 def file_create(path: str, content='') -> str:
     assert not os.path.exists(path), f'already exists: {path}'
-    content = content.rstrip() + '\n'
-    with open(path, mode='w', encoding='utf8', newline='\n') as fp:
+    content = content.rstrip() + NEWLINE
+    with open(path, mode='w', encoding='utf8', newline=NEWLINE) as fp:
         fp.write(content)
 
 

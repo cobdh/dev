997e207e421f9b1593995552152d7d300c591622
Helmut Konrad Schewe <helmutus@outlook.com>
2023-03-07 11:57:09 +0000

    feat(utils): add method to sort files by file name
    + extend public API
    + add unit tests

--- cobdh/__init__.py
+++ cobdh/__init__.py
@@ -3,8 +3,11 @@ from cobdh.utils import NEWLINE
 from cobdh.utils import SUCCESS
 from cobdh.utils import file_create
 from cobdh.utils import file_list
+from cobdh.utils import file_name
 from cobdh.utils import file_read
 from cobdh.utils import file_replace
+from cobdh.utils import files_sort
+from cobdh.utils import forward_slash
 from cobdh.utils import run
 
 __version__ = '0.1.0'
--- cobdh/utils.py
+++ cobdh/utils.py
@@ -1,5 +1,7 @@
+import contextlib
 import os
 import pathlib
+import re
 import subprocess
 
 SUCCESS = 0
@@ -42,6 +44,88 @@ def file_list(path, exts: str = 'xml'):
         for ext in exts.split():
             result += list(path.glob(f'**/*.{ext}'))
     return result
+
+
+def file_name(path: str, ext: bool = False) -> str:
+    """Determine file name without file extension out of file path.
+
+    >>> file_name('/etc/profile.d/helm.sh')
+    'helm'
+    >>> file_name('info.txt')
+    'info'
+    >>> file_name('/etc/.tmp')
+    '.tmp'
+    >>> file_name('.etc')
+    '.etc'
+    >>> file_name('/no/file/ext')
+    'ext'
+    >>> file_name('etc/dev/raw.png', ext=True)
+    'raw.png'
+    """
+    assert path, path
+    path = forward_slash(path)
+    try:
+        _, name = path.rsplit('/', 1)
+    except ValueError:
+        name = path
+    if ext:
+        return name
+    if name[0] == '.':
+        return name
+    result = name.split('.')[0]
+    return result
+
+
+def files_sort(files: list) -> list:
+    """Sort `files` path alphabetically. Sort file names by number if given.
+
+    >>> files_sort(('/c/a', '/c/200.txt', '/c/2.txt', '/c/3', '/c/0.bmp'))
+    ['/c/0.bmp', '/c/2.txt', '/c/3', '/c/200.txt', '/c/a']
+    """
+    files = [forward_slash(item) for item in files]
+
+    def number_filename(item):
+        # sort file names if they are numbers: 0,1,2,3,4,5,6,7,8,9,10
+        item = item.lower()
+        item = file_name(item)
+        with contextlib.suppress(ValueError):
+            # sort items by number
+            item = int(item)
+            # ensure to compare str and str and not str and int
+            item = str(item).zfill(20)
+        return item
+
+    files = sorted(files, key=number_filename)
+    return files
+
+
+BACKSLASH = re.compile(r'\\')
+NL = re.compile(r'\\(?!n)')
+
+
+def forward_slash(content: str, keep_newline: bool = False) -> str:
+    r"""Replace every backward slash \\ with an forward slash /.
+
+    Args:
+        content(str): content with backslash's
+        keep_newline(bool): if True, do not convert \n to /n
+    Returns:
+        content without backslash's
+
+    Examples:
+    >>> forward_slash('\\helm\nelm', keep_newline=True)
+    '/helm\nelm'
+    >>> forward_slash('\\helm\\telm', keep_newline=True)
+    '/helm/telm'
+    >>> forward_slash('\\helm\\nelm')
+    '/helm/nelm'
+    """
+    assert isinstance(content, str), str(content)
+    pattern = BACKSLASH
+    if keep_newline:
+        pattern = NL
+    content = re.sub(pattern, '/', content)
+    return content
 
 
 def run(cmd: str, cwd: str = None, expect=True):

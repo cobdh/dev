d860e3cd795f6bc96d3c18ec5c4b779cbbc05f2b
Helmut Konrad Schewe <helmutus@outlook.com>
2023-03-13 11:22:58 +0000

    feat(API): add method to convert xml to str

--- cobdh/__init__.py
+++ cobdh/__init__.py
@@ -11,6 +11,7 @@ from cobdh.utils import file_replace
 from cobdh.utils import files_sort
 from cobdh.utils import forward_slash
 from cobdh.utils import run
+from cobdh.xml.inter import to_str as xml_tostr
 from cobdh.xml.parser import parse as xml_parse
 
 __version__ = '0.1.0'

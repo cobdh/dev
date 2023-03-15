17ce155c5a1b6c9be6535efd0aab60152030f788
Helmut Konrad Schewe <helmutus@outlook.com>
2023-03-15 14:08:49 +0000

    feat(crawl): add cli to parse complete portal

---
+++ cobdh/crawl.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import collections
+import functools
+import os
+import re
+import urllib.error
+import urllib.parse
+import urllib.request
+
+import werkzeug.urls
+
+import cobdh
+
+# front end
+BASE = os.environ['SERVER_TEST'].rstrip('/')
+
+# TODO: IMPROVE VIA GOOGLE
+HREF = re.compile(r'a[ ]{1,4}href[ ]{0,4}=[ ]{0,4}[\"\'](.+?)[\"\']')
+
+
+def urls(root: str = BASE, deep: int = 5) -> list:
+    result = collections.defaultdict(set)
+    result[root] = set()
+    if deep < 0:
+        return result
+    try:
+        content = curl(root)
+    except urllib.error.HTTPError:
+        print(f'could not curl: {root}')
+        result[root].add('[OFFLINE]')
+        return result
+    for hyperhyper in HREF.findall(content):
+        if hyperhyper == '/':
+            continue
+        hyperhyper = werkzeug.urls.url_fix(hyperhyper)
+        hyperhyper = abspath(root, hyperhyper)
+        if hyperhyper in result:
+            result[hyperhyper].add(root.rstrip('/'))
+            continue
+        result[hyperhyper].add(root.rstrip('/'))
+        children = urls(
+            root=hyperhyper,
+            deep=deep - 1,
+        )
+        for key, value in children.items():
+            result[key] |= value
+    return result
+
+
+def abspath(root, hyper):
+    if urllib.parse.urlsplit(hyper).netloc:
+        # already abspath
+        return hyper
+    hyper = hyper.strip()
+    # ensure to start with /
+    hyper = '/' + hyper.lstrip('/')
+    # TODO: THERE MUST BE A BETTER WAY
+    splitted = urllib.parse.urlsplit(root)
+    root = splitted.scheme + '://' + splitted.netloc
+    result = f'{root}{hyper}'
+    return result
+
+
+@functools.lru_cache(maxsize=2048)
+def curl(path: str):
+    url = werkzeug.urls.url_fix(path)
+    with urllib.request.urlopen(url) as response:  # nosec
+        html = response.read()
+    html: str = html.decode('utf8')
+    return html
+
+
+def main() -> int:
+    result = urls(deep=5)
+    print()
+    for key, value in result.items():
+        print(f'{key}\n\t{value}\n')
+    return cobdh.SUCCESS
+
+
+if __name__ == "__main__":
+    main()

3fea301be80558065363aa2bf13be74625eb4086
Helmut Konrad Schewe <helmutus@outlook.com>
2023-05-21 13:06:54 +0000

    chore(alias): add live shortcut

--- short/alias.sh
+++ short/alias.sh
@@ -3,6 +3,7 @@ cd $COBDH
 alias fapp="cd ${COBDH}/app"
 alias fdat="cd ${COBDH}/collection"
 alias fdev="cd ${COBDH}/dev"
+alias fliv="cd ${COBDH}/live"
 alias fndb="cd ${COBDH}/nano_db"
 alias fnpu="cd ${COBDH}/nano_pub"
 alias fpla="cd ${COBDH}/plan"
